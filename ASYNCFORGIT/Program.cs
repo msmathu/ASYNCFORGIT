f11123711ab99f772b407f93dde56d3a851bd52a
MADHU SUTHANAN M <79738641+msmathu@users.noreply.github.com>
2023-06-21 20:51:24 +0530

    Update Program.cs

--- ASYNCFORGIT/Program.cs
+++ ASYNCFORGIT/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+  
+class Program
+{
+
+
+    static async Task Main(string[] args)
+    {
+        var result1 = await LongProcess1();
+        Console.WriteLine(result1);
+        Console.ReadKey();
+    }
+
+    static async Task<string> LongProcess1()
+    {
+        Console.WriteLine("LongProcess 1 Started");
+
+        await Task.Delay(4000);
+
+        Console.WriteLine("LongProcess 1 Completed");
+
+        return "method 1";
+    }
+
+   
+
+    
+}
